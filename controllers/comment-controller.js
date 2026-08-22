18407abb137114c507eac7ee7bea9cf0e4e09d39
Adil Ishtiaq <adilishtiaq034@gmail.com>
2026-08-22 17:08:34 +0500

    Creating Comment Part Completed

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -3,10 +3,20 @@ const Comment = require('../models/comment-model')
 const createComment = async function(req,res){
 
  const postId = req.params.postId
+ const userId = req.user.userId
+ const {comment} = req.body
 
+ if(postId && userId && comment){
+       const newComment = Comment.create({
+        post:postId,
+        comment:comment,
+        createdBy:userId
+       })
+       res.status(201).json({message:'Comment created successfully',newComment})
 
-
-
-
-
-}
+ }
+ else{
+    return res.status(400).json({message:'Please provide all required fields'})
+ }
+ } 
+ 
