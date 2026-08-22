0391c168479a7b1055b9c42c8fd79b4028075d8e
Adil Ishtiaq <adilishtiaq034@gmail.com>
2026-08-22 16:59:23 +0500

    Setting Up Comments Part

---
+++ models/comments-model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const commentSchema = new mongoose.Schema({
+     
+    post:{
+             type:mongoose.Schema.Types.ObjectId,
+             ref: 'Post',
+             required: true
+    },
+    comment:{
+             type:String,
+             maxlength:100,
+             minlength:1,
+             trim:true,
+             required:true
+    },
+    createdBy:{
+             type:mongoose.Schema.Types.ObjectId,
+             ref: "Auth",
+             required:true
+}
+},
+{timestamps:true})
