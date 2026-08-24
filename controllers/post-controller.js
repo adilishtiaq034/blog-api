d35a9ec76f40ab2ab785a826dc095e8adf44a3f5
Adil Ishtiaq <adilishtiaq034@gmail.com>
2026-08-24 18:33:03 +0500

    Added Pagination

--- controllers/post-controller.js
+++ controllers/post-controller.js
@@ -17,7 +17,23 @@ const createPost = async function(req,res){
 
 const getAllPosts = async function(req,res){
     try{
-        const posts = await Post.find().populate('createdBy', 'name email')
+            
+        const page = Number(req.query.page) || 1
+        const limit = Number(req.query.limit) || 10
+        const skip = (page - 1) * limit
+
+
+
+
+
+
+
+
+
+
+
+
+        const posts = await Post.find().populate('createdBy', 'name email').skip(skip).limit(limit)
         res.status(200).json({
             message: 'Posts retrieved successfully',
             posts: posts
