a903c6fb5fc0652625e58cb64d2781e2e0e32de3
Adil Ishtiaq <adilishtiaq034@gmail.com>
2026-09-01 18:49:54 +0500

    Cleaning up app.js

--- app.js
+++ app.js
@@ -2,19 +2,12 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 require('dotenv').config()
-const mongoose = require('mongoose')
 const postRouter = require('./routers/post-router')
 const authRouter = require('./routers/user-routes')
 const commentRouter = require('./routers/comment-router')
 const likeRouter = require('./routers/like-router')
 
-mongoose.connect(process.env.mongodb_uri)
-.then(()=>{
-    console.log('Connected to MongoDB')
-})
-.catch((err)=>{
-    console.error('Error connecting to MongoDB:', err)
-})
+
 app.use('/uploads', express.static('uploads'))
 
 app.use('/blog', postRouter)
